5eee9ae5e0012ebcacf8d6f591f2843b8146e11b
Hoorain-kamal <hkamal9380@gmail.com>
2025-11-11 08:53:49 +0530

    Add files via upload

---
+++ cprogram21.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("Enter the number: ");
+    scanf("%d",&x);
+    if(x>0)
+    {
+        printf("%d is positive\n",x);
+    }
+
+    return 0;
+}
